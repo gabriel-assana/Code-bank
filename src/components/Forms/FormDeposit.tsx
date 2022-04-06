41f0b7a7c32d388f4dd449545181b8e36ffc93f8
Gabriel Assana <gabrielhhz2@gmail.com>
2022-04-05 22:17:21 -0300

    Criação da Função handleCreateDeposit

--- src/components/Forms/FormDeposit.tsx
+++ src/components/Forms/FormDeposit.tsx
@@ -1,24 +1,45 @@
 import '../../styles/form/Form.css';
 
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 export function FormDeposit(){
 
+    const [valueDeposit, setValueDeposit] = useState(0)
+    const [descriptionDeposit, setDescriptionDeposit] = useState('')
+
+
+    function handleCreateDeposit(event: FormEvent) {
+        event.preventDefault();      
+
+        console.log(
+            valueDeposit,
+            descriptionDeposit
+        )
+    }
+
+
     return(
         <>
-            <form>
+            <form onSubmit={handleCreateDeposit}>
 
                 <h2>Depositos</h2>
 
                 <input 
+                    type="number"
                     placeholder="Valor"
+                    value={valueDeposit}
+                    onChange={event => setValueDeposit(Number(event.target.value))}
                 />
 
                 <input 
                     placeholder="Descrição do deposito"
+                    value={descriptionDeposit}
+                    onChange={event => setDescriptionDeposit(event.target.value)}
                 />
                 
-                <button type="submit">Depositar</button>
+                <button type="submit">
+                    Depositar
+                </button>
             </form>
 
         </>
